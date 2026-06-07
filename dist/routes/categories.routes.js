baea4dadab6c8f58cb251a6a6b72a6af879e0574
wilps3 <wilberps3@gmail.com>
2026-06-07 11:12:52 -0500

    Delete dist directory

--- dist/routes/categories.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const categories_controller_1 = require("../controllers/categories.controller");
-const router = (0, express_1.Router)();
-router.get("/", categories_controller_1.getCategories);
-exports.default = router;
--- dist/routes/coworking.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const coworking_controller_1 = require("../controllers/coworking.controller");
-const router = (0, express_1.Router)();
-router.get("/", coworking_controller_1.getCoworkingSpaces);
-router.get("/:id", coworking_controller_1.getCoworkingSpaceById);
-exports.default = router;
